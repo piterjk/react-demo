9da2036bcc8c9eeb501e8b8e95de04497f133ada
PiterJK <piterjk@naver.com>
2025-01-25 10:01:30 +0900

    basename 설정

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     return (
       <div className="App">
           <AuthProvider>
-              <BrowserRouter>
+              <BrowserRouter basename="/react-demo">
                   <Header/>
                   <div className="App-container">
                       <AuthenticatedRoute>
